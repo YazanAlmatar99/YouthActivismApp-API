1674faa82f95e124e36744469f966930a55cb2d4
Yazan <yazanmatar90@gmail.com>
2021-01-28 20:24:56 -0500

    Profile model initialize

---
+++ models/Profile.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+    },
+  company: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('profile', ProfileSchema);
